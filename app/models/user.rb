5c1c982bd20d84f7b028a08ae39e85f3f27476cf
Brennan M. O'Keefe <brennan@considered-harmful.org>
2012-07-06 18:46:30 -0500

    Add micropost model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
   
   before_save { self.email.downcase! }
   before_save :create_remember_token
