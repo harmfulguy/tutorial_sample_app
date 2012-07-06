c67d9912bd3ab841c4652bb7044a5051a990a0f8
Brennan M. O'Keefe <brennan@considered-harmful.org>
2012-07-06 13:20:53 -0500

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
     sequence(:email) { |n| "person_#{n}@exapmle.com" }
     password "foobar"
     password_confirmation "foobar"
+    
+    factory :admin do
+      admin true
+    end
   end
 end
