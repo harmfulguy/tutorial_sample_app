6e516fd58180b75b5a5a798e2c9baca57300817d
Brennan M. O'Keefe <brennan@considered-harmful.org>
2012-07-09 12:04:18 -0500

    Follow/unfollow buttons

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -59,7 +59,7 @@ describe "Authentication" do
         end
         
         describe "after signing in" do
-          
+
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
@@ -103,6 +103,18 @@ describe "Authentication" do
         
         describe "submitting to the destroy action" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+      
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+        
+        describe "submitting ot the destroy action" do
+          before { delete relationship_path(1) }
           specify { response.should redirect_to(signin_path) }
         end
       end
